0a0c439cff7a28c418ec814f544c5c357c97d46b
Unknown <achrafatauil@hotmail.com>
2022-05-31 13:23:11 +0200

    WIP add form retrieval

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import { app, errorHandler } from 'mu';
-import { getSemanticForm } from './lib/getSemanticForm';
+import { app, errorHandler, uuid } from 'mu';
+import { createForm } from './lib/createForm';
+import { getForm } from './lib/getForm';
 
 app.get('/', function(req, res) {
   const message = `Hey there, you have reached the lpdc-management-service! Seems like I\'m doing just fine, have a nice day! :)`;
@@ -20,7 +21,7 @@ app.post('/semantic-forms/', async function(req, res) {
   });
   
   try {
-    const { uuid, uri } = await getSemanticForm(publicServiceId);
+    const { uuid, uri } = await createForm(publicServiceId);
     return res.status(201).json({
       data: {
         "type": "public-service",
@@ -28,6 +29,28 @@ app.post('/semantic-forms/', async function(req, res) {
         "uri": uri
       }
     })
+  } catch (e) {
+    console.error(e);
+    if (e.status) {
+      return res.status(e.status).set('content-type', 'application/json').send(e);
+    }
+    const response = {
+      status: 500,
+      message: `Something unexpected went wrong while submitting semantic-form for "${uuid}".`
+    };
+    return res.status(response.status).set('content-type', 'application/json').send(response.message);
+  }
+});
+
+app.get('/semantic-forms/', async function(req, res) {
+
+  const publicServiceId = req.params["public-service-id"];
+  const formId = req.params["form-id"];
+  
+  try {
+    const bundle = await getForm(publicServiceId, formId)
+
+    return res.status(201).json(bundle)
   } catch (e) {
     console.error(e);
     if (e.status) {
