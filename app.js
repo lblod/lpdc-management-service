0b9d2256db4d95f60eae8eea2f37ae1f0151157f
Unknown <achrafatauil@hotmail.com>
2022-06-02 13:21:39 +0200

    add put & delete endpoints

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { app, errorHandler, uuid } from 'mu';
 import { createForm } from './lib/createForm';
 import { getForm } from './lib/getForm';
+import { updateForm } from './lib/updateForm';
 
 app.get('/', function(req, res) {
   const message = `Hey there, you have reached the lpdc-management-service! Seems like I\'m doing just fine, have a nice day! :)`;
@@ -42,13 +43,13 @@ app.post('/semantic-forms/', async function(req, res) {
   }
 });
 
-app.get('/semantic-forms/', async function(req, res) {
+app.get('/semantic-forms/:publicServiceId/form/:formId', async function(req, res) {
 
-  const publicServiceId = req.params["public-service-id"];
-  const formId = req.params["form-id"];
+  const publicServiceId = req.params["publicServiceId"];
+  const formId = req.params["formId"];
   
   try {
-    const bundle = await getForm(publicServiceId, formId)
+    const bundle = await getForm(publicServiceId, formId);
 
     return res.status(201).json(bundle)
   } catch (e) {
@@ -59,6 +60,44 @@ app.get('/semantic-forms/', async function(req, res) {
     const response = {
       status: 500,
       message: `Something unexpected went wrong while submitting semantic-form for "${uuid}".`
+    };
+    return res.status(response.status).set('content-type', 'application/json').send(response.message);
+  }
+});
+
+app.put('/semantic-forms/:formId', async function(req, res) {
+
+  const formId = req.params.formId;
+  const delta = req.body;
+  try {
+    await updateForm(formId, delta);
+    return res.status(201).json("")
+  } catch (e) {
+    console.error(e);
+    if (e.status) {
+      return res.status(e.status).set('content-type', 'application/json').send(e);
+    }
+    const response = {
+      status: 500,
+      message: `Something unexpected went wrong while submitting semantic-form for "${uuid}".`
+    };
+    return res.status(response.status).set('content-type', 'application/json').send(response.message);
+  }
+});
+
+app.delete('/semantic-forms/:formId', async function(req, res) {
+  const formId = req.params.formId;
+  try {
+    await deleteForm(formId);
+    return res.status(204).send();
+  } catch (e) {
+    console.error(e);
+    if (e.status) {
+      return res.status(e.status).set('content-type', 'application/json').send(e);
+    }
+    const response = {
+      status: 500,
+      message: `Something unexpected went wrong while deleting the semantic-form for "${uuid}".`
     };
     return res.status(response.status).set('content-type', 'application/json').send(response.message);
   }
