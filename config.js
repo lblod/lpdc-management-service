beaedc1316c3dae96d38dba88e9d01e46f07ae89
Unknown <achrafatauil@hotmail.com>
2022-06-08 11:50:42 +0200

    semi-colons

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
-const FORM_STATUS_ACTIVE = 'http://data.lblod.info/id/concept/ipdc-thema/27d784f8-e7ea-4f18-88f8-02d427033009'
-const FORM_STATUS_STOPPED = 'http://data.lblod.info/id/concept/ipdc-thema/12873bfb-d2cc-4cc5-9255-1bf0ca2747f9'
-const FORM_STATUS_CONCEPT = 'http://lblod.data.gift/concepts/79a52da4-f491-4e2f-9374-89a13cde8ecd'
+const FORM_STATUS_ACTIVE = 'http://data.lblod.info/id/concept/ipdc-thema/27d784f8-e7ea-4f18-88f8-02d427033009';
+const FORM_STATUS_STOPPED = 'http://data.lblod.info/id/concept/ipdc-thema/12873bfb-d2cc-4cc5-9255-1bf0ca2747f9';
+const FORM_STATUS_CONCEPT = 'http://lblod.data.gift/concepts/79a52da4-f491-4e2f-9374-89a13cde8ecd';
 
 
 export {
