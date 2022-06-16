3a7ac0d1151cb6a9da80d29385c53b4179b6a5e3
Unknown <achrafatauil@hotmail.com>
2022-06-16 11:30:11 +0200

    create & use APPLICATION_GRAPH env

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 const FORM_STATUS_ACTIVE = 'http://data.lblod.info/id/concept/ipdc-thema/27d784f8-e7ea-4f18-88f8-02d427033009';
 const FORM_STATUS_STOPPED = 'http://data.lblod.info/id/concept/ipdc-thema/12873bfb-d2cc-4cc5-9255-1bf0ca2747f9';
 const FORM_STATUS_CONCEPT = 'http://lblod.data.gift/concepts/79a52da4-f491-4e2f-9374-89a13cde8ecd';
+const APPLICATION_GRAPH = 'http://mu.semte.ch/graphs/application';
 
 const NESTED_PREDICATES = [
   "http://vocab.belgif.be/ns/publicservice#hasRequirement",
@@ -21,6 +22,7 @@ export {
   FORM_STATUS_ACTIVE,
   FORM_STATUS_STOPPED,
   FORM_STATUS_CONCEPT,
+  APPLICATION_GRAPH,
   NESTED_PREDICATES,
   FORM_MAPPING
 };
