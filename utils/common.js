781b795cd53d1f1616ac002365c34185f4615dce
felix <ruiz.felixruiz@gmail.com>
2023-03-10 11:17:54 +0100

    Extrat common function

--- utils/common.js
+++ utils/common.js
@@ -1,4 +1,5 @@
 import { v4 as uuid } from 'uuid';
+import { querySudo } from '@lblod/mu-auth-sudo';
 
 export function addUuidForSubject(bindings, newUuid = uuid()) {
   if(bindings.length) {
@@ -53,4 +54,17 @@ export function replaceType(bindings, oldType, newType) {
     }
   }
   return bindings;
+}
+
+export async function getScopedGraphsForStatement( statement, targetGraphPattern ) {
+  const queryStr = `
+    SELECT DISTINCT ?g {
+      GRAPH ?g {
+        ${statement}
+      }
+      FILTER(REGEX(STR(?g), "^${targetGraphPattern}", "i"))
+   }
+  `;
+  const result = await querySudo(queryStr);
+  return result?.results?.bindings.map(b => b.g.value) || [];
 }
