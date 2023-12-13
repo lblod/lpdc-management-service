bbc808ccc7272f9d4e2a2b7296ca7c15c39e6659
joren <joren.kennes@kunlabora.be>
2023-12-13 09:44:15 +0100

    Revert "LPDC-821: add script that add virtuoso en runs tests on ci"
    This reverts commit fa972922fb0d850d230d7ec6a39d02f0b2c69a5b.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    transform: {"^.+\\.[t|j]sx?$": "ts-jest"},
-    globalSetup: './test/test-setup.ts'
+    transform: {"^.+\\.[t|j]sx?$": "ts-jest"}
 };
