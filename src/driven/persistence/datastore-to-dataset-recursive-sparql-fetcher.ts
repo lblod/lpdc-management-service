fd5e49aaffd036b611191374d185ea223f255442
gauquiebart <bart.gauquie@gmail.com>
2023-12-27 13:13:23 +0100

    LPDC-916: support typed-literal + prevent one to many recursion in a loop

--- src/driven/persistence/datastore-to-dataset-recursive-sparql-fetcher.ts
+++ src/driven/persistence/datastore-to-dataset-recursive-sparql-fetcher.ts
@@ -3,7 +3,7 @@
 import {Iri} from "../../core/domain/shared/iri";
 import {sparqlEscapeUri} from "../../../mu-helper";
 import {SparqlQuerying} from "./sparql-querying";
-import {Literal, literal, namedNode, NamedNode, quad} from "rdflib";
+import {isNamedNode, Literal, literal, namedNode, NamedNode, quad} from "rdflib";
 import {Quad} from "rdflib/lib/tf-types";
 
 export class DatastoreToDatasetRecursiveSparqlFetcher {
@@ -15,47 +15,71 @@ export class DatastoreToDatasetRecursiveSparqlFetcher {
     }
 
     async fetch(graph: Iri, from: Iri): Promise<Quad[]> {
+        return await this.doFetch(graph, [from], []);
+    }
+
+    private async doFetch(graph: Iri, subjectIds: Iri[], previouslyQueriedIds: Iri[]): Promise<Quad[]> {
+        if(subjectIds.length === 0) {
+            return [];
+        }
         const query =
             ` SELECT ?s ?p ?o
               WHERE { 
                     GRAPH ${sparqlEscapeUri(graph)} {
                         VALUES(?s) {
-                            (${sparqlEscapeUri(from)})
+                            ${subjectIds.map(subjectId => `(${sparqlEscapeUri(subjectId)}) `).join(' ')}
                         } 
                         ?s ?p ?o
                     }
               }`;
 
-        console.log(query);
-
         const result = await this.querying.list(query);
-        const quads = [];
-        result.map(r => {
+        const quads = this.asQuads(result, graph);
+
+        const referencedIds =
+            quads
+                .filter(q => isNamedNode(q.object)
+                    && q.predicate.value !== `http://www.w3.org/1999/02/22-rdf-syntax-ns#type`
+                    && !previouslyQueriedIds.some(id => id === q.object.value))
+                .map(q => q.object.value);
+
+        const otherIdsToQuery = new Set(referencedIds);
+        subjectIds.forEach(subjectId => otherIdsToQuery.delete(subjectId));
+        previouslyQueriedIds.forEach(previouslyQueriedId => otherIdsToQuery.delete(previouslyQueriedId));
+
+        return [...quads, ...await this.doFetch(graph, [...otherIdsToQuery], [...subjectIds, ...previouslyQueriedIds])];
+    }
+
+    private asQuads(queryResults: unknown[], graph: string) {
+        return queryResults.map(r => {
             const s = this.asNamedNode(r['s']);
             const p = this.asNamedNode(r['p']);
             const o = this.asNamedNodeOrLiteral(r['o']);
             const g = namedNode(graph);
-            quads.push(quad(s, p, o, g));
+            return quad(s, p, o, g);
         });
-
-        return quads;
     }
 
     private asNamedNodeOrLiteral(term: any): NamedNode | Literal {
         if (term.type === 'uri') {
             return this.asNamedNode(term);
         }
-        if (term.type === 'literal') {
+        if (term.type === 'literal'
+                || term.type === 'typed-literal') {
             return this.asLiteral(term);
         }
-        throw new Error(`Could not parse ${term} as Named Node Or Literal`);
+        throw new Error(`Could not parse ${JSON.stringify(term)} as Named Node Or Literal`);
     }
 
     private asLiteral(term: any): Literal {
-        if (term.type !== 'literal') {
-            throw new Error(`Expecting an literal for ${term.value}`);
+        if (term.type !== 'literal'
+                && term.type !== 'typed-literal') {
+            throw new Error(`Expecting a literal for ${term.value}`);
         }
-        return literal(term.value);
+
+        const lang: string | undefined = term['xml:lang'];
+        const datatype: string | undefined = term['datatype'];
+        return literal(term.value, lang || datatype);
     }
 
     private asNamedNode(term: any): NamedNode {
--- test/driven/persistence/datastore-to-dataset-recursive-sparql-fetcher.it-test.ts
+++ test/driven/persistence/datastore-to-dataset-recursive-sparql-fetcher.it-test.ts
@@ -1,6 +1,6 @@
 import {DirectDatabaseAccess} from "./direct-database-access";
 import {TEST_SPARQL_ENDPOINT} from "../../test.config";
-import {uuid} from "../../../mu-helper";
+import {sparqlEscapeDate, sparqlEscapeDateTime, sparqlEscapeInt, uuid} from "../../../mu-helper";
 import {PREFIX} from "../../../config";
 import {
     DatastoreToDatasetRecursiveSparqlFetcher
@@ -23,22 +23,255 @@ describe('recursively fetches a part of a datastore into a dataset using sparql 
     });
 
     test('Can query non recursively all triples linked to an IRI', async () => {
-        const anUUID = uuid();
-        const startIri: Iri = `http://example.com/ns#${anUUID}`;
+        const startIri: Iri = `http://example.com/ns#${uuid()}`;
         const graph: Iri = 'http://some-test-graph';
+        const aDateTime = new Date('2022-08-13T15:36:52Z');
+        const aDate = new Date('2023-10-28');
 
         await directDatabaseAccess.insertData(
             graph,
             [`<${startIri}> a ex:aType`,
                 `<${startIri}> ex:aPredicate1 """aStringValue"""`,
+                `<${startIri}> ex:aPredicate2 """aStringValue-no-lang"""`,
+                `<${startIri}> ex:aPredicate2 """aStringValue-nl"""@nl`,
+                `<${startIri}> ex:aPredicate2 """aStringValue-en"""@en`,
+                `<${startIri}> ex:aPredicate3 ${sparqlEscapeDateTime(aDateTime.toISOString())}`,
+                `<${startIri}> ex:aPredicate4 ${sparqlEscapeDate(aDate.toISOString())}`,
+                `<${startIri}> ex:aPredicate5 ${sparqlEscapeInt(1200)}`,
+            ],
+            [PREFIX.ex]);
+
+        const otherStartIri: Iri = `http://example.com/ns#${uuid()}`;
+        await directDatabaseAccess.insertData(
+            graph,
+            [`<${otherStartIri}> a ex:anOtherType`,
+            ],
+            [PREFIX.ex]);
+
+
+        const actualDataSet: Quad[] = await fetcher.fetch('http://some-test-graph', startIri);
+
+        const expectedQuads = [
+            quad(namedNode(startIri), namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), namedNode("http://example.com/ns#aType"), namedNode(graph)),
+            quad(namedNode(startIri), namedNode('http://example.com/ns#aPredicate1'), literal('aStringValue', "http://www.w3.org/2001/XMLSchema#string"), namedNode(graph)),
+            quad(namedNode(startIri), namedNode('http://example.com/ns#aPredicate2'), literal('aStringValue-no-lang', "http://www.w3.org/2001/XMLSchema#string"), namedNode(graph)),
+            quad(namedNode(startIri), namedNode('http://example.com/ns#aPredicate2'), literal('aStringValue-nl', "nl"), namedNode(graph)),
+            quad(namedNode(startIri), namedNode('http://example.com/ns#aPredicate2'), literal('aStringValue-en', "en"), namedNode(graph)),
+            quad(namedNode(startIri), namedNode('http://example.com/ns#aPredicate3'), literal('2022-08-13T15:36:52Z', "http://www.w3.org/2001/XMLSchema#dateTime"), namedNode(graph)),
+            quad(namedNode(startIri), namedNode('http://example.com/ns#aPredicate4'), literal('2023-10-28', "http://www.w3.org/2001/XMLSchema#date"), namedNode(graph)),
+            quad(namedNode(startIri), namedNode('http://example.com/ns#aPredicate5'), literal('1200', "http://www.w3.org/2001/XMLSchema#integer"), namedNode(graph)),
+        ];
+
+        expect(actualDataSet).toEqual(expect.arrayContaining(expectedQuads));
+        expect(actualDataSet).toHaveLength(expectedQuads.length);
+    });
+
+    test('Can query one level deep for a one-to-one relationship and retrieve all triples linked to an IRI', async () => {
+        const startIri: Iri = `http://example.com/ns#${uuid()}`;
+        const nestedIri: Iri = `http://example.com/ns#${uuid()}`;
+        const graph: Iri = 'http://some-test-graph';
+
+        await directDatabaseAccess.insertData(
+            graph,
+            [`<${startIri}> a ex:aType`,
+                `<${startIri}> ex:has-one-to-one <${nestedIri}>`,
+                `<${nestedIri}> ex:aPredicate1 """aStringValue"""`,
             ],
             [PREFIX.ex]);
 
         const actualDataSet: Quad[] = await fetcher.fetch('http://some-test-graph', startIri);
 
-        expect(actualDataSet).toEqual([quad(namedNode(startIri), namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), namedNode("http://example.com/ns#aType"), namedNode(graph)),
-                                                quad(namedNode(startIri), namedNode('http://example.com/ns#aPredicate1'), literal('aStringValue'), namedNode(graph))]);
-
+        const expectedQuads = [
+            quad(namedNode(startIri), namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), namedNode("http://example.com/ns#aType"), namedNode(graph)),
+            quad(namedNode(startIri), namedNode('http://example.com/ns#has-one-to-one'), namedNode(nestedIri), namedNode(graph)),
+            quad(namedNode(nestedIri), namedNode('http://example.com/ns#aPredicate1'), literal('aStringValue'), namedNode(graph)),
+        ];
+        expect(actualDataSet).toEqual(expect.arrayContaining(expectedQuads));
+        expect(actualDataSet).toHaveLength(expectedQuads.length);
     });
+
+    test('Can query one level deep for a one-to-many relationship and retrieve all triples linked to an IRI', async () => {
+        const startIri: Iri = `http://example.com/ns#${uuid()}`;
+        const nestedIri: Iri = `http://example.com/ns#${uuid()}`;
+        const nestedIri2: Iri = `http://example.com/ns#${uuid()}`;
+        const graph: Iri = 'http://some-test-graph';
+
+        await directDatabaseAccess.insertData(
+            graph,
+            [`<${startIri}> a ex:aType`,
+                `<${startIri}> ex:has-one-to-many <${nestedIri}>`,
+                `<${nestedIri}> ex:aPredicate1 """aStringValue"""`,
+                `<${startIri}> ex:has-one-to-many <${nestedIri2}>`,
+                `<${nestedIri2}> ex:aPredicate2 """aStringValue2"""`,
+            ],
+            [PREFIX.ex]);
+
+        const actualDataSet: Quad[] = await fetcher.fetch('http://some-test-graph', startIri);
+
+        const expectedQuads = [
+            quad(namedNode(startIri), namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), namedNode("http://example.com/ns#aType"), namedNode(graph)),
+            quad(namedNode(startIri), namedNode('http://example.com/ns#has-one-to-many'), namedNode(nestedIri), namedNode(graph)),
+            quad(namedNode(nestedIri), namedNode('http://example.com/ns#aPredicate1'), literal('aStringValue'), namedNode(graph)),
+            quad(namedNode(startIri), namedNode('http://example.com/ns#has-one-to-many'), namedNode(nestedIri2), namedNode(graph)),
+            quad(namedNode(nestedIri2), namedNode('http://example.com/ns#aPredicate2'), literal('aStringValue2'), namedNode(graph)),
+        ];
+        expect(actualDataSet).toEqual(expect.arrayContaining(expectedQuads));
+        expect(actualDataSet).toHaveLength(expectedQuads.length);
+    });
+
+    test('Can query multiple levels deep for a one-to-one relationship and retrieve all triples linked to an IRI', async () => {
+        const startIri: Iri = `http://example.com/ns#${uuid()}`;
+        const nestedIri: Iri = `http://example.com/ns#${uuid()}`;
+        const nestedNestedIri: Iri = `http://example.com/ns#${uuid()}`;
+        const graph: Iri = 'http://some-test-graph';
+
+        await directDatabaseAccess.insertData(
+            graph,
+            [`<${startIri}> a ex:aType`,
+                `<${startIri}> ex:has-one-to-one <${nestedIri}>`,
+                `<${nestedIri}> ex:aPredicate1 """aStringValue"""`,
+                `<${nestedIri}> ex:has-other-one-to-one <${nestedNestedIri}>`,
+                `<${nestedNestedIri}> ex:aPredicate2 """aStringValue2"""`,
+            ],
+            [PREFIX.ex]);
+
+        const actualDataSet: Quad[] = await fetcher.fetch('http://some-test-graph', startIri);
+
+        const expectedQuads = [
+            quad(namedNode(startIri), namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), namedNode("http://example.com/ns#aType"), namedNode(graph)),
+            quad(namedNode(startIri), namedNode('http://example.com/ns#has-one-to-one'), namedNode(nestedIri), namedNode(graph)),
+            quad(namedNode(nestedIri), namedNode('http://example.com/ns#aPredicate1'), literal('aStringValue'), namedNode(graph)),
+            quad(namedNode(nestedIri), namedNode('http://example.com/ns#has-other-one-to-one'), namedNode(nestedNestedIri), namedNode(graph)),
+            quad(namedNode(nestedNestedIri), namedNode('http://example.com/ns#aPredicate2'), literal('aStringValue2'), namedNode(graph)),
+        ];
+        expect(actualDataSet).toEqual(expect.arrayContaining(expectedQuads));
+        expect(actualDataSet).toHaveLength(expectedQuads.length);
+    });
+
+    test('Can query one level deep for a one-to-many relationship and retrieve all triples linked to an IRI', async () => {
+        const startIri: Iri = `http://example.com/ns#${uuid()}`;
+        const nestedIri: Iri = `http://example.com/ns#${uuid()}`;
+        const nestedNestedIri: Iri = `http://example.com/ns#${uuid()}`;
+        const nested2NestedIri: Iri = `http://example.com/ns#${uuid()}`;
+        const nested2Iri: Iri = `http://example.com/ns#${uuid()}`;
+        const nestedNested2Iri: Iri = `http://example.com/ns#${uuid()}`;
+        const nested2nested2Iri: Iri = `http://example.com/ns#${uuid()}`;
+        const graph: Iri = 'http://some-test-graph';
+
+        await directDatabaseAccess.insertData(
+            graph,
+            [`<${startIri}> a ex:aType`,
+                `<${startIri}> ex:has-one-to-many <${nestedIri}>`,
+                `<${nestedIri}> ex:aPredicate1 """aStringValue"""`,
+                `<${nestedIri}> ex:has-other-one-to-many <${nestedNestedIri}>`,
+                `<${nestedNestedIri}> ex:aPredicate1 """aStringValue33"""`,
+                `<${nestedIri}> ex:has-other-one-to-many <${nested2NestedIri}>`,
+                `<${nested2NestedIri}> ex:aPredicate1 """aStringValue64"""`,
+                `<${startIri}> ex:has-one-to-many <${nested2Iri}>`,
+                `<${nested2Iri}> ex:aPredicate2 """aStringValue2"""`,
+                `<${nested2Iri}> ex:has-other-other-one-to-many <${nestedNested2Iri}>`,
+                `<${nestedNested2Iri}> ex:aPredicate1 """aStringValue6687"""`,
+                `<${nested2Iri}> ex:has-other-other-one-to-many <${nested2nested2Iri}>`,
+                `<${nested2nested2Iri}> ex:aPredicate1 """aStringValue67977"""`,
+            ],
+            [PREFIX.ex]);
+
+        const actualDataSet: Quad[] = await fetcher.fetch('http://some-test-graph', startIri);
+
+        const expectedQuads = [
+            quad(namedNode(startIri), namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), namedNode("http://example.com/ns#aType"), namedNode(graph)),
+            quad(namedNode(startIri), namedNode('http://example.com/ns#has-one-to-many'), namedNode(nestedIri), namedNode(graph)),
+            quad(namedNode(nestedIri), namedNode('http://example.com/ns#aPredicate1'), literal('aStringValue'), namedNode(graph)),
+            quad(namedNode(nestedIri), namedNode('http://example.com/ns#has-other-one-to-many'), namedNode(nestedNestedIri), namedNode(graph)),
+            quad(namedNode(nestedNestedIri), namedNode('http://example.com/ns#aPredicate1'), literal('aStringValue33'), namedNode(graph)),
+            quad(namedNode(nestedIri), namedNode('http://example.com/ns#has-other-one-to-many'), namedNode(nested2NestedIri), namedNode(graph)),
+            quad(namedNode(nested2NestedIri), namedNode('http://example.com/ns#aPredicate1'), literal('aStringValue64'), namedNode(graph)),
+            quad(namedNode(startIri), namedNode('http://example.com/ns#has-one-to-many'), namedNode(nested2Iri), namedNode(graph)),
+            quad(namedNode(nested2Iri), namedNode('http://example.com/ns#aPredicate2'), literal('aStringValue2'), namedNode(graph)),
+            quad(namedNode(nested2Iri), namedNode('http://example.com/ns#has-other-other-one-to-many'), namedNode(nestedNested2Iri), namedNode(graph)),
+            quad(namedNode(nestedNested2Iri), namedNode('http://example.com/ns#aPredicate1'), literal('aStringValue6687'), namedNode(graph)),
+            quad(namedNode(nested2Iri), namedNode('http://example.com/ns#has-other-other-one-to-many'), namedNode(nested2nested2Iri), namedNode(graph)),
+            quad(namedNode(nested2nested2Iri), namedNode('http://example.com/ns#aPredicate1'), literal('aStringValue67977'), namedNode(graph)),
+        ];
+        expect(actualDataSet).toEqual(expect.arrayContaining(expectedQuads));
+        expect(actualDataSet).toHaveLength(expectedQuads.length);
+    });
+
+    test('Can query a start triple referencing its own', async () => {
+        const startIri: Iri = `http://example.com/ns#${uuid()}`;
+        const graph: Iri = 'http://some-test-graph';
+
+        await directDatabaseAccess.insertData(
+            graph,
+            [`<${startIri}> a ex:aType`,
+                    `<${startIri}> ex:has-one-to-one <${startIri}>`,
+            ],
+            [PREFIX.ex]);
+
+        const actualDataSet: Quad[] = await fetcher.fetch('http://some-test-graph', startIri);
+
+        const expectedQuads = [
+            quad(namedNode(startIri), namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), namedNode("http://example.com/ns#aType"), namedNode(graph)),
+            quad(namedNode(startIri), namedNode('http://example.com/ns#has-one-to-one'), namedNode(startIri), namedNode(graph)),
+        ];
+        expect(actualDataSet).toEqual(expect.arrayContaining(expectedQuads));
+        expect(actualDataSet).toHaveLength(expectedQuads.length);
+    });
+
+    test('Can query a loop', async () => {
+        const startIri: Iri = `http://example.com/ns#${uuid()}`;
+        const nestedIri: Iri = `http://example.com/ns#${uuid()}`;
+        const nestedNestedIri: Iri = `http://example.com/ns#${uuid()}`;
+        const graph: Iri = 'http://some-test-graph';
+
+        await directDatabaseAccess.insertData(
+            graph,
+            [`<${startIri}> ex:has-one-to-one <${nestedIri}>`,
+                    `<${nestedIri}> ex:has-other-one-to-one <${nestedNestedIri}>`,
+                    `<${nestedNestedIri}> ex:has-other-other-one-to-one <${startIri}>`,
+            ],
+            [PREFIX.ex]);
+
+        const actualDataSet: Quad[] = await fetcher.fetch('http://some-test-graph', startIri);
+
+        const expectedQuads = [
+            quad(namedNode(startIri), namedNode('http://example.com/ns#has-one-to-one'), namedNode(nestedIri), namedNode(graph)),
+            quad(namedNode(nestedIri), namedNode('http://example.com/ns#has-other-one-to-one'), namedNode(nestedNestedIri), namedNode(graph)),
+            quad(namedNode(nestedNestedIri), namedNode('http://example.com/ns#has-other-other-one-to-one'), namedNode(startIri), namedNode(graph)),
+        ];
+        expect(actualDataSet).toEqual(expect.arrayContaining(expectedQuads));
+        expect(actualDataSet).toHaveLength(expectedQuads.length);
+    });
+
+    test('Stops at graph boundary', async () => {
+        const startIri: Iri = `http://example.com/ns#${uuid()}`;
+        const nestedIri: Iri = `http://example.com/ns#${uuid()}`;
+        const nestedNestedIri: Iri = `http://example.com/ns#${uuid()}`;
+        const graph: Iri = 'http://some-test-graph';
+        const otherGraph: Iri = 'http://some-other-test-graph';
+
+        await directDatabaseAccess.insertData(
+            graph,
+            [`<${startIri}> ex:has-one-to-one <${nestedIri}>`,
+                `<${nestedIri}> ex:has-other-one-to-one <${nestedNestedIri}>`,
+            ],
+            [PREFIX.ex]);
+
+        await directDatabaseAccess.insertData(
+            otherGraph,
+            [`<${nestedNestedIri}> ex:has-other-other-one-to-one <${startIri}>`,
+            ],
+            [PREFIX.ex]);
+
+        const actualDataSet: Quad[] = await fetcher.fetch('http://some-test-graph', startIri);
+
+        const expectedQuads = [
+            quad(namedNode(startIri), namedNode('http://example.com/ns#has-one-to-one'), namedNode(nestedIri), namedNode(graph)),
+            quad(namedNode(nestedIri), namedNode('http://example.com/ns#has-other-one-to-one'), namedNode(nestedNestedIri), namedNode(graph)),
+        ];
+        expect(actualDataSet).toEqual(expect.arrayContaining(expectedQuads));
+        expect(actualDataSet).toHaveLength(expectedQuads.length);
+    });
+
 
 });
