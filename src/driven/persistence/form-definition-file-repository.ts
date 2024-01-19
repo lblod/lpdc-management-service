266a136ce804616182df8b9737cf7955cdd831d6
gauquiebart <bart.gauquie@gmail.com>
2024-01-19 13:51:46 +0100

    LPDC-917: improve performance by caching the resolved static form definitions

--- src/driven/persistence/form-definition-file-repository.ts
+++ src/driven/persistence/form-definition-file-repository.ts
@@ -5,18 +5,30 @@ import {FORM_MAPPING} from "../../../config";
 //TODO LPDC-917: write it tests (instead of only current end to end tests)
 export class FormDefinitionFileRepository implements FormDefinitionRepository {
 
-    //TODO LPDC-917: switch language type to Language
-    loadFormDefinition(formId: string, language: string, isEnglishRequired: boolean): string {
+    //TODO LPDC-917: create a small object that caches the results (in a general way) - and create a cache key using the args ...
+    private readonly cachedDefinitions = {};
 
+    //TODO LPDC-917: switch language type to Language
+    public loadFormDefinition(formId: string, language: string, isEnglishRequired: boolean): string {
+        const cacheKey = `${formId}-${language}-${isEnglishRequired}`;
+        if(this.cachedDefinitions[cacheKey]) {
+            return this.cachedDefinitions[cacheKey];
+        }
+
+        const result = this.doReadLoadFormDefinition(formId, isEnglishRequired, language);
+
+        this.cachedDefinitions[cacheKey] = result;
+
+        return result;
+    }
+
+    private doReadLoadFormDefinition(formId: string, isEnglishRequired: boolean, language: string) {
         let form = fs.readFileSync(`./src/driven/persistence/forms/${FORM_MAPPING[formId]}/form.ttl`, 'utf8');
-        // If a user chooses "YourEurope" as their publication channel, load
-        // the relevants snippets into the content form that render the English fields obligatory.
         if (FORM_MAPPING[formId] === "content" && isEnglishRequired) {
             const englishRequirementFormSnippets = fs.readFileSync(`./src/driven/persistence/forms/${FORM_MAPPING[formId]}/add-english-requirement.ttl`, 'utf8');
             form += englishRequirementFormSnippets;
         }
-        form = this.adjustLanguageOfForm(form, language);
-        return form;
+        return this.adjustLanguageOfForm(form, language);
     }
 
     private adjustLanguageOfForm(form: string, newLanguage: string): string {
