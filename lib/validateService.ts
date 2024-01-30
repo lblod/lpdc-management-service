0c9316439779178905df169cf9a3fd387b3509cd
gauquiebart <bart.gauquie@gmail.com>
2024-01-30 14:27:27 +0100

    LPDC-917: move TODO's to LPDC-1014

--- lib/validateService.ts
+++ lib/validateService.ts
@@ -83,7 +83,7 @@ export async function validateService(instanceId: Iri, bestuurseenheid: Bestuurs
     return response;
 }
 
-//TODO LPDC-917: use domain to validate ...
+//TODO LPDC-1014: use domain to validate ...
 async function validateAddresses(serviceUri: string): Promise<boolean> {
     const addresses = await loadContactPointsAddresses(serviceUri, {type: 'cpsv:PublicService', includeUuid: true});
     if (addresses) {
