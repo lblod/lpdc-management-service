886f9960e846d512de672761f940a75de0093551
joren <joren.kennes@kunlabora.be>
2024-02-26 08:19:04 +0100

    LPDC-910: add check for all triples used instance-snapshot data integrity test

--- test/data-integrity-validation/instance-snapshot-data-integrity.it-test.ts
+++ test/data-integrity-validation/instance-snapshot-data-integrity.it-test.ts
@@ -5,16 +5,21 @@ import {PREFIX, PUBLIC_GRAPH} from "../../config";
 import {sparqlEscapeUri} from "../../mu-helper";
 import {Iri} from "../../src/core/domain/shared/iri";
 import {BestuurseenheidSparqlTestRepository} from "../driven/persistence/bestuurseenheid-sparql-test-repository";
+import {DomainToQuadsMapper} from "../../src/driven/persistence/domain-to-quads-mapper";
+import {namedNode, Statement} from "rdflib";
+import {sortedUniq, uniq} from "lodash";
+import {SparqlQuerying} from "../../src/driven/persistence/sparql-querying";
 
 describe('Instance Snapshot Data Integrity Validation', () => {
 
     const endPoint = END2END_TEST_SPARQL_ENDPOINT; //Note: replace by END2END_TEST_SPARQL_ENDPOINT to verify all
 
+    const sparqlQuerying = new SparqlQuerying(endPoint);
     const repository = new InstanceSnapshotSparqlTestRepository(endPoint);
     const bestuurseenheidRepository = new BestuurseenheidSparqlTestRepository(endPoint);
     const directDatabaseAccess = new DirectDatabaseAccess(endPoint);
 
-    test.skip('Query for each bestuurseenheid the instance snapshots in the ldes input graph', async () => {
+    test('Query for each bestuurseenheid the instance snapshots in the ldes input graph', async () => {
         const bestuurseenheidsQuery = `
             ${PREFIX.besluit}
             SELECT ?id WHERE {
@@ -41,21 +46,50 @@ describe('Instance Snapshot Data Integrity Validation', () => {
                 `;
                 const instanceSnapshotIds = (await directDatabaseAccess.list(query)).map(result => new Iri(result['id'].value));
 
+                const allTriplesOfGraphQuery = `
+                    ${PREFIX.lpdcExt}
+                    SELECT ?s ?p ?o WHERE {
+                        GRAPH ${sparqlEscapeUri(bestuurseenheid.instanceSnapshotsLdesDataGraph())} {
+                            ?s ?p ?o
+                        }
+                    }`;
+
+                const allTriplesOfGraph = await directDatabaseAccess.list(allTriplesOfGraphQuery);
+                let allQuadsOfGraph: Statement[] = uniq(sparqlQuerying.asQuads(allTriplesOfGraph, bestuurseenheid.instanceSnapshotsLdesDataGraph().value));
+
+                // filter out processed snapshots list
+                allQuadsOfGraph = allQuadsOfGraph.filter(q => !q.predicate.equals(namedNode('http://mu.semte.ch/vocabularies/ext/processed')));
+
+                // filter out ldes state
+                allQuadsOfGraph = allQuadsOfGraph.filter(q => !q.predicate.equals(namedNode('http://mu.semte.ch/vocabularies/ext/state')));
+
+
+                let quadsForQueriedInstanceSnapshots: Statement[] = [];
+
                 for (const instanceSnapshotId of instanceSnapshotIds) {
                     try {
 
                         const instanceSnapshot = await repository.findById(bestuurseenheid, instanceSnapshotId);
                         expect(instanceSnapshot.id).toEqual(instanceSnapshotId);
 
-                        //TODO LPDC-910: also verify if all quads are mapped
+                        const quadsForInstanceSnapshotForId = new DomainToQuadsMapper(bestuurseenheid.instanceSnapshotsLdesDataGraph())
+                            .instanceSnapshotToQuads(instanceSnapshot);
+                        quadsForQueriedInstanceSnapshots = [...quadsForInstanceSnapshotForId, ...quadsForQueriedInstanceSnapshots];
 
                     } catch (e) {
                         console.error(instanceSnapshotId);
                         console.error(e);
                         dataErrors.push(e);
                     }
-
                 }
+
+                const quadsFromRequeriedInstanceSnapshotsAsStrings = quadsForQueriedInstanceSnapshots.map(quad => quad.toString());
+
+                const allRemainingQuadsOfGraphAsTurtle = allQuadsOfGraph
+                    .map(q => q.toString())
+                    .filter(q => !quadsFromRequeriedInstanceSnapshotsAsStrings.includes(q));
+
+                expect(sortedUniq(allRemainingQuadsOfGraphAsTurtle)).toEqual([]);
 
             } catch (e) {
                 console.log(e);
