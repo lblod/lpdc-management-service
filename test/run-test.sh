ed1d90b44b574d3457b08473b201def731c25cd9
joren <joren.kennes@kunlabora.be>
2024-04-02 16:15:11 +0200

    fix exit code run-test.sh script

--- test/run-test.sh
+++ test/run-test.sh
@@ -5,9 +5,18 @@ cd ..
 echo "Running lint & tests"
 npm ci
 npm run lint
+exit_code_lint=$?
 npm run test
+exit_code_test=$?
+
+if [ $exit_code_lint -eq 1 ] || [ $exit_code_test -eq 1 ]; then
+    exit_code=1
+else
+    exit_code=0
+fi
 
 echo "Stopping containers"
 docker compose -p lpdc-management-service-tests down
 
-cd test || exit
+cd test
+exit exit_code
