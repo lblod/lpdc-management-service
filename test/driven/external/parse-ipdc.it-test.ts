09bc803570eb411757b4d72a13c2b50ba9bdb0bd
gauquiebart <bart.gauquie@gmail.com>
2024-04-23 09:50:32 +0200

    LPDC-1139: remove chat gpt example

--- test/driven/external/parse-ipdc.it-test.ts
+++ test/driven/external/parse-ipdc.it-test.ts
@@ -1,4 +1,4 @@
-import {graph, log, parse, sym} from 'rdflib';
+import {graph, parse} from 'rdflib';
 import {aBestuurseenheid} from "../../core/domain/bestuurseenheid-test-builder";
 import {
     DoubleQuadReporter,
@@ -11,45 +11,7 @@ import {Iri} from "../../../src/core/domain/shared/iri";
 describe('Parse ipdc', () => {
 
 
-    test('chat gpt example', async (done) => {
-        // Example JSON-LD data
-        const jsonldData = `
-        {
-          "@context": {
-            "name": "http://xmlns.com/foaf/0.1/name",
-            "homepage": {
-              "@id": "http://xmlns.com/foaf/0.1/homepage",
-              "@type": "@id"
-            }
-          },
-          "@id": "http://example.org/person",
-          "name": "John Doe",
-          "homepage": "http://johndoe.com"
-        }`;
-
-        // Create an empty RDF graph
-        const store = graph();
-
-        // Define the MIME type for JSON-LD
-        const contentType = 'application/ld+json';
-
-        // Define a base URI for the data
-        const baseUri = 'http://example.org/';
-
-        // Parse the JSON-LD data into the graph
-        parse(jsonldData, store, baseUri, contentType, callback =>  {
-            console.log('callback called');
-            // Querying the data to retrieve the name of the person
-            const nameTerm = sym('http://example.org/person');
-            const namePredicate = sym('http://xmlns.com/foaf/0.1/name');
-            const name = store.any(nameTerm, namePredicate, undefined);
-
-            console.log(`Name: ${name?.value}`);  // Output: Name: John Doe
-            done();
-        });
-    });
-
-    test('parse ipdc example', async (done) => {
+   test('parse ipdc example', async (done) => {
         const bestuurseenheid = aBestuurseenheid().build();
         const res = await fetch('https://productencatalogus-v3.vlaanderen.be/doc/instantie/f7d287bf-7adb-434e-a33e-7fe28818fd99', {
             headers: {'Accept': 'application/ld+json'}
@@ -72,6 +34,7 @@ describe('Parse ipdc', () => {
 
             const quads = kb.statementsMatching();
 
+            //don't use parsing to instance , use expose inner methods
             const instance = new QuadsToDomainMapper(quads, bestuurseenheid.userGraph(), doubleQuadReporter)
                 .instance(new Iri('http://data.lblod.info/id/public-service/f7d287bf-7adb-434e-a33e-7fe28818fd99'));
 
