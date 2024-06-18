c189acf60e2ee0cded59eac92636e6fa3007db97
gauquiebart <bart.gauquie@gmail.com>
2024-06-18 16:55:36 +0200

    LPDC-1002: remove unused code

--- lib/commonQueries.ts
+++ lib/commonQueries.ts
@@ -1,52 +1,5 @@
-import {query, sparqlEscapeUri} from '../mu-helper';
-import {querySudo} from '@lblod/mu-auth-sudo';
-import {APPLICATION_GRAPH, PREFIXES} from '../config';
+import {query} from '../mu-helper';
 import {sortBy} from "lodash";
-
-export async function loadContactPointsAddresses(service: string, {
-    graph,
-    type,
-    includeUuid,
-    sudo,
-    connectionOptions
-}: QueryOptions = {}): Promise<any[]> {
-    graph = graph || APPLICATION_GRAPH;
-    type = type || 'lpdcExt:ConceptualPublicService';
-    includeUuid = includeUuid || false;
-    const queryClient = sudo ? querySudo : query;
-    connectionOptions = connectionOptions || {};
-
-    const queryStr = `
-    ${PREFIXES}
-    SELECT DISTINCT ?s ?p ?o
-      WHERE  {
-        VALUES ?p {
-          rdf:type
-          ${includeUuid ? 'mu:uuid' : ''}
-          adres:Straatnaam
-          <https://data.vlaanderen.be/ns/adres#Adresvoorstelling.huisnummer>
-          <https://data.vlaanderen.be/ns/adres#Adresvoorstelling.busnummer>
-          adres:postcode
-          adres:gemeentenaam
-          adres:land
-          dct:source
-          sh:order
-          adres:verwijstNaar
-        }
-
-        GRAPH ${sparqlEscapeUri(graph)} {
-          ${sparqlEscapeUri(service)} a ${type};
-            m8g:hasContactPoint ?contact.
-
-          ?contact a schema:ContactPoint;
-                lpdcExt:address ?s.
-
-          ?s ?p ?o.
-         }
-      }
-  `;
-    return (await queryClient(queryStr, {}, connectionOptions)).results.bindings;
-}
 
 //TODO LPDC-1014: move to domain
 export async function loadContactPointOption(option: string): Promise<any> {
@@ -59,13 +12,4 @@ export async function loadContactPointOption(option: string): Promise<any> {
           }
     `)).results.bindings.map((object) => object.option.value);
     return sortBy(unsortedContactPointOptions, (option) => option.toUpperCase());
-}
-
-type QueryOptions = {
-    graph?: string,
-    type?: string,
-    validTypes?: boolean,
-    includeUuid?: boolean,
-    sudo?: boolean,
-    connectionOptions?: object,
 }
