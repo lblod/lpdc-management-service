9a01aab962b2aef9ea7430ba7ebd100b98249e1d
thuysbr <brent.thuys@kunlabora.be>
2024-07-03 12:55:02 +0200

    LPDC-1213: Add logic to calculate needConversionFromFormalToInformal

---
+++ migration-scripts/transfer-instances/transfer-instance-service.ts
@@ -0,0 +1,72 @@
+import {Instance, InstanceBuilder} from "../../src/core/domain/instance";
+import {BestuurseenheidRepository} from "../../src/core/port/driven/persistence/bestuurseenheid-repository";
+import {Iri} from "../../src/core/domain/shared/iri";
+import {InstanceRepository} from "../../src/core/port/driven/persistence/instance-repository";
+import {FormalInformalChoiceRepository} from "../../src/core/port/driven/persistence/formal-informal-choice-repository";
+import {ChosenFormType, CompetentAuthorityLevelType, InstanceStatusType} from "../../src/core/domain/types";
+import {InvariantError} from "../../src/core/domain/shared/lpdc-error";
+import {uuid} from "../../mu-helper";
+import {Language} from "../../src/core/domain/language";
+import {FormatPreservingDate} from "../../src/core/domain/format-preserving-date";
+
+export class TransferInstanceService {
+
+    private readonly bestuurseenheidRepository: BestuurseenheidRepository;
+    private readonly instanceRepository: InstanceRepository;
+    private formalInformalChoiceRepository: FormalInformalChoiceRepository;
+
+    constructor(bestuurseenheidRepository: BestuurseenheidRepository, instanceRepository: InstanceRepository, formalInformalChoiceRepository: FormalInformalChoiceRepository) {
+        this.bestuurseenheidRepository = bestuurseenheidRepository;
+        this.instanceRepository = instanceRepository;
+        this.formalInformalChoiceRepository = formalInformalChoiceRepository;
+    }
+
+    async transfer(instanceId: Iri, fromAuthorityId: Iri, toAuthorityId: Iri): Promise<Instance> {
+
+        const fromAuthority = await this.bestuurseenheidRepository.findById(fromAuthorityId);
+        const toAuthority = await this.bestuurseenheidRepository.findById(toAuthorityId);
+
+        const toAuthorityChoice = (await this.formalInformalChoiceRepository.findByBestuurseenheid(toAuthority))?.chosenForm;
+
+        const instance: Instance = await this.instanceRepository.findById(fromAuthority, instanceId);
+
+        if (toAuthorityChoice === ChosenFormType.FORMAL && instance.dutchLanguageVariant === Language.INFORMAL) {
+            throw new InvariantError("transforming informal instance to formal is not possible");
+        }
+        return this.copyInstance(toAuthority.id, toAuthorityChoice, instance);
+
+    }
+
+    private copyInstance(toAuthorityId: Iri, toAuthorityChoice: ChosenFormType, instanceToCopy: Instance) {
+        const instanceUuid = uuid();
+        const instanceId = InstanceBuilder.buildIri(instanceUuid);
+
+        const hasCompetentAuthorityLevelLokaal = instanceToCopy.competentAuthorityLevels.includes(CompetentAuthorityLevelType.LOKAAL);
+        const needsConversionFromFormalToInformal = (toAuthorityChoice === ChosenFormType.INFORMAL && instanceToCopy.dutchLanguageVariant !== Language.INFORMAL);
+
+        return InstanceBuilder.from(instanceToCopy)
+            .withId(instanceId)
+            .withUuid(instanceUuid)
+            .withCreatedBy(toAuthorityId)
+            .withDateCreated(FormatPreservingDate.now())
+            .withDateModified(FormatPreservingDate.now())
+            .withRequirements(instanceToCopy.requirements.map(req => req.transformWithNewId()))
+            .withProcedures(instanceToCopy.procedures.map(proc => proc.transformWithNewId()))
+            .withWebsites(instanceToCopy.websites.map(ws => ws.transformWithNewId()))
+            .withCosts(instanceToCopy.costs.map(c => c.transformWithNewId()))
+            .withFinancialAdvantages(instanceToCopy.financialAdvantages.map(fa => fa.transformWithNewId()))
+            .withContactPoints(instanceToCopy.contactPoints.map(fa => fa.transformWithNewId()))
+            .withStatus(InstanceStatusType.ONTWERP)
+            .withDateSent(undefined)
+            .withPublicationStatus(undefined)
+            .withDatePublished(undefined)
+            .withNeedsConversionFromFormalToInformal(needsConversionFromFormalToInformal)
+            .withLegalResources(instanceToCopy.legalResources.map(lr => lr.transformWithNewId()))
+            .withSpatials(instanceToCopy.forMunicipalityMerger ? [] : instanceToCopy.spatials)
+            .withExecutingAuthorities(instanceToCopy.forMunicipalityMerger ? [] : instanceToCopy.executingAuthorities)
+            .withCompetentAuthorities(instanceToCopy.forMunicipalityMerger && hasCompetentAuthorityLevelLokaal ? [] : instanceToCopy.competentAuthorities)
+            .withForMunicipalityMerger(false)
+            .withCopyOf(instanceToCopy.id)
+            .build();
+    }
+}
---
+++ migration-scripts/transfer-instances/transfer-instances-script.ts
@@ -0,0 +1,78 @@
+import {DirectDatabaseAccess} from "../../test/driven/persistence/direct-database-access";
+import {PREFIX} from "../../config";
+import {sparqlEscapeUri} from "../../mu-helper";
+import {Bestuurseenheid} from "../../src/core/domain/bestuurseenheid";
+import {Iri} from "../../src/core/domain/shared/iri";
+import {InstanceSparqlRepository} from "../../src/driven/persistence/instance-sparql-repository";
+import fs from "fs";
+import {BestuurseenheidSparqlRepository} from "../../src/driven/persistence/bestuurseenheid-sparql-repository";
+import {DomainToQuadsMapper} from "../../src/driven/persistence/domain-to-quads-mapper";
+import {BestuurseenheidTestBuilder} from "../../test/core/domain/bestuurseenheid-test-builder";
+import {
+    FormalInformalChoiceSparqlRepository
+} from "../../src/driven/persistence/formal-informal-choice-sparql-repository";
+import {TransferInstanceService} from "./transfer-instance-service";
+
+const endPoint = process.env.SPARQL_URL;
+const directDatabaseAccess = new DirectDatabaseAccess(endPoint);
+const bestuurseenheidRepository = new BestuurseenheidSparqlRepository(endPoint);
+const instanceRepository = new InstanceSparqlRepository(endPoint);
+const formalInformalChoiceRepository = new FormalInformalChoiceSparqlRepository(endPoint);
+
+const transferInstanceService = new TransferInstanceService(bestuurseenheidRepository, instanceRepository, formalInformalChoiceRepository);
+
+async function main(fromAuthorityId: Iri, toAuthorityId: Iri, onlyForMunicipalityMergerInstances: boolean) {
+    const insertQuads = [];
+    const fromAuthority = await bestuurseenheidRepository.findById(fromAuthorityId);
+    const toAuthority = await bestuurseenheidRepository.findById(toAuthorityId);
+    const domainToQuadsMerger = new DomainToQuadsMapper(toAuthority.userGraph());
+
+    const instanceIds: Iri[] = onlyForMunicipalityMergerInstances ? await getAllInstanceIdsWithMunicipalityMergerForBestuurseenheid(fromAuthority) : await getAllInstanceIdsForBestuurseenheid(fromAuthority);
+
+    console.log(`Instances to transfer: ${instanceIds.length}`);
+    for (const instanceId of instanceIds) {
+        const newInstance = await transferInstanceService.transfer(instanceId, fromAuthorityId, toAuthorityId);
+
+        const quads = domainToQuadsMerger.instanceToQuads(newInstance).map(quad => quad.toCanonical()).join('\n');
+        insertQuads.push(quads);
+    }
+    fs.writeFileSync(`./migration-results/transfer-instances.ttl`, insertQuads.join('\n'));
+    console.log("instances done " + insertQuads.length);
+}
+
+async function getAllInstanceIdsForBestuurseenheid(bestuurseenheid: Bestuurseenheid): Promise<Iri[]> {
+    const query = `
+            ${PREFIX.lpdcExt}
+            SELECT ?id WHERE {
+                GRAPH ${sparqlEscapeUri(bestuurseenheid.userGraph())} {
+                    ?id a lpdcExt:InstancePublicService .
+                }
+            }
+            `;
+    const instanceIds = await directDatabaseAccess.list(query);
+    return instanceIds.map(instanceId => new Iri(instanceId['id'].value));
+}
+
+async function getAllInstanceIdsWithMunicipalityMergerForBestuurseenheid(bestuurseenheid: Bestuurseenheid): Promise<Iri[]> {
+    const query = `
+            ${PREFIX.lpdcExt}
+            SELECT ?id WHERE {
+                GRAPH ${sparqlEscapeUri(bestuurseenheid.userGraph())} {
+                    ?id a lpdcExt:InstancePublicService .
+                    ?id lpdcExt:forMunicipalityMerger """true"""^^<http://www.w3.org/2001/XMLSchema#boolean>
+                }
+            }
+            `;
+    const instanceIds = await directDatabaseAccess.list(query);
+    return instanceIds.map(instanceId => new Iri(instanceId['id'].value));
+}
+
+const pepingen = BestuurseenheidTestBuilder.PEPINGEN_IRI;
+const borgloon = BestuurseenheidTestBuilder.BORGLOON_IRI;
+
+
+const fromAuthority = new Iri(pepingen.value);
+const toAuthority = new Iri(borgloon.value);
+
+
+main(fromAuthority, toAuthority, true);
