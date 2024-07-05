868a4a356c74731bde705f3ec930d7f098646ae1
thuysbr <brent.thuys@kunlabora.be>
2024-07-05 17:07:48 +0200

    LPDC-1241: adapt script after removing publicationStatus

--- migration-scripts/transfer-instances/archive-instances-script.ts
+++ migration-scripts/transfer-instances/archive-instances-script.ts
@@ -1,15 +1,15 @@
-import {sparqlEscapeDateTime, sparqlEscapeUri} from "../../mu-helper";
+import {sparqlEscapeDateTime, sparqlEscapeUri, uuid} from "../../mu-helper";
 import {Iri} from "../../src/core/domain/shared/iri";
 import {InstanceSparqlRepository} from "../../src/driven/persistence/instance-sparql-repository";
 import fs from "fs";
 import {BestuurseenheidSparqlRepository} from "../../src/driven/persistence/bestuurseenheid-sparql-repository";
 import {DomainToQuadsMapper} from "../../src/driven/persistence/domain-to-quads-mapper";
 import {BestuurseenheidTestBuilder} from "../../test/core/domain/bestuurseenheid-test-builder";
-import {NS} from "../../src/driven/persistence/namespaces";
-import {InstancePublicationStatusType} from "../../src/core/domain/types";
 import {Bestuurseenheid} from "../../src/core/domain/bestuurseenheid";
+import {PublishedInstanceSnapshotBuilder} from "../../src/core/domain/published-instance-snapshot";
 
 const endPoint = process.env.SPARQL_URL;
+
 const bestuurseenheidRepository = new BestuurseenheidSparqlRepository(endPoint);
 const instanceRepository = new InstanceSparqlRepository(endPoint);
 
@@ -17,18 +17,18 @@ const instanceRepository = new InstanceSparqlRepository(endPoint);
 async function main(bestuurseenheidId: Iri) {
     const insertQuads = [];
     const deleteQuads = [];
-    const bestuurseeheid = await bestuurseenheidRepository.findById(bestuurseenheidId);
+    const bestuurseenheid = await bestuurseenheidRepository.findById(bestuurseenheidId);
 
-    const domainToQuadsMerger = new DomainToQuadsMapper(bestuurseeheid.userGraph());
+    const domainToQuadsMerger = new DomainToQuadsMapper(bestuurseenheid.userGraph());
 
     const instanceIds: string[] = fs.readFileSync(`migration-results/initial-instances.ttl`, 'utf8').split('\n');
 
 
     console.log(`Instances to archive: ${instanceIds.length}`);
     for (const instanceId of instanceIds) {
-        const instance = await instanceRepository.findById(bestuurseeheid, new Iri(instanceId));
+        const instance = await instanceRepository.findById(bestuurseenheid, new Iri(instanceId));
 
-        if (instance.publicationStatus) {
+        if (instance.dateSent !== undefined) {
             const quads = tombstoneQuads(instance.id.value);
             insertQuads.push(quads);
         }
@@ -36,16 +36,23 @@ async function main(bestuurseenheidId: Iri) {
         const quads = domainToQuadsMerger.instanceToQuads(instance).map(quad => quad.toNT()).join('\n');
         deleteQuads.push(quads);
     }
-    createSparql(bestuurseeheid, insertQuads, deleteQuads);
+    createSparql(bestuurseenheid, insertQuads, deleteQuads);
 }
 
 function tombstoneQuads(instanceId: string) {
+
+    const uniqueId = uuid();
+    const tombstoneId = PublishedInstanceSnapshotBuilder.buildIri(uniqueId);
+
+
     const tombstoneQuads = [];
     const now = new Date();
-    tombstoneQuads.push(`${sparqlEscapeUri(instanceId)} a <https://www.w3.org/ns/activitystreams#Tombstone> .`);
-    tombstoneQuads.push(`${sparqlEscapeUri(instanceId)} <https://www.w3.org/ns/activitystreams#formerType> <https://productencatalogus.data.vlaanderen.be/ns/ipdc-lpdc#InstancePublicService> .`);
-    tombstoneQuads.push(`${sparqlEscapeUri(instanceId)} <https://www.w3.org/ns/activitystreams#deleted> ${sparqlEscapeDateTime(now)} .`);
-    tombstoneQuads.push(`${sparqlEscapeUri(instanceId)} <http://schema.org/publication> ${NS.concepts.publicationStatus(InstancePublicationStatusType.TE_HERPUBLICEREN)} .`);
+    tombstoneQuads.push(`${sparqlEscapeUri(tombstoneId)} a <https://www.w3.org/ns/activitystreams#Tombstone> .`);
+    tombstoneQuads.push(`${sparqlEscapeUri(tombstoneId)} <https://www.w3.org/ns/activitystreams#formerType> <https://productencatalogus.data.vlaanderen.be/ns/ipdc-lpdc#InstancePublicService> .`);
+    tombstoneQuads.push(`${sparqlEscapeUri(tombstoneId)} <https://productencatalogus.data.vlaanderen.be/ns/ipdc-lpdc#isPublishedVersionOf> ${sparqlEscapeUri(instanceId)} .`);
+    tombstoneQuads.push(`${sparqlEscapeUri(tombstoneId)} <https://www.w3.org/ns/activitystreams#deleted> ${sparqlEscapeDateTime(now)} .`);
+    tombstoneQuads.push(`${sparqlEscapeUri(tombstoneId)} <http://www.w3.org/ns/prov#generatedAtTime> ${sparqlEscapeDateTime(now)} .`);
+
 
     return tombstoneQuads.join(`\n`);
 }
@@ -68,7 +75,6 @@ function createSparql(bestuurseenheid: Bestuurseenheid, insertQuads, deleteQuads
 
     fs.writeFileSync(`./migration-results/archive-instances.sparql`, query);
 }
-
 
 const pepingen = BestuurseenheidTestBuilder.PEPINGEN_IRI;
 
