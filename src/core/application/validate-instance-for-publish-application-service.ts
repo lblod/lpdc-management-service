6182cdc09f3b51256aed4facb19601bdbca8bf97
onodrim <alexander.van.den.berghe@redpencil.io>
2025-01-20 09:26:24 +0100

    feat: check whether instance has expired spatials before publishing
    Add an extra validation to the publish instance functionality to ensure all
    assigned spatials are still valid. This means that each spatial
    - does **not** have an end date; or
    - has an end date that is in the future.

--- src/core/application/validate-instance-for-publish-application-service.ts
+++ src/core/application/validate-instance-for-publish-application-service.ts
@@ -9,23 +9,29 @@ import { InvariantError, NotFoundError } from "../domain/shared/lpdc-error";
 import { Instance } from "../domain/instance";
 import { ENABLE_ADDRESS_VALIDATION } from "../../../config";
 import { BestuurseenheidRepository } from "../port/driven/persistence/bestuurseenheid-repository";
+import { SpatialRepository } from "../port/driven/persistence/spatial-repository";
 
 export const INACTIVE_AUTHORITY_ERROR_MESSAGE =
   "Het product dat je probeert te verzenden, is gelinkt aan een inactief bestuur. Kijk in de tab ‘eigenschappen’ de overheidsorganisatie na.";
+export const EXPIRED_SPATIAL_ERROR_MESSAGE =
+  "Het product dat je probeert te verzenden, is gelinkt aan een inactief bestuur. Kijk in de tab ‘eigenschappen’ het geografisch toepassingsgebied na.";
 
 export class ValidateInstanceForPublishApplicationService {
   private readonly _formApplicationService: FormApplicationService;
   private readonly _instanceRepository: InstanceRepository;
   private readonly _bestuurseenheidRepository: BestuurseenheidRepository;
+  private readonly _spatialRepository: SpatialRepository;
 
   constructor(
     formApplicationService: FormApplicationService,
     instanceRepository: InstanceRepository,
     bestuurseenheidRepository: BestuurseenheidRepository,
+    spatialRepository: SpatialRepository,
   ) {
     this._formApplicationService = formApplicationService;
     this._instanceRepository = instanceRepository;
     this._bestuurseenheidRepository = bestuurseenheidRepository;
+    this._spatialRepository = spatialRepository;
   }
 
   async validate(
@@ -36,42 +42,79 @@ export class ValidateInstanceForPublishApplicationService {
       instanceId,
       bestuurseenheid,
     );
+
     if (errorList.length) {
       return errorList;
-    } else {
-      const instance = await this._instanceRepository.findById(
-        bestuurseenheid,
-        instanceId,
+    }
+
+    const instance = await this._instanceRepository.findById(
+      bestuurseenheid,
+      instanceId,
+    );
+
+    const authorityError = await this.validateAuthorities(instance);
+    if (authorityError) {
+      errorList.push(authorityError);
+    }
+
+    const spatialError = await this.validateSpatials(instance);
+    if (spatialError) {
+      errorList.push(spatialError);
+    }
+
+    if (errorList.length) {
+      return errorList;
+    }
+
+    return this.tryValidateForPublish(instance);
+  }
+
+  private async validateAuthorities(
+    instance: Instance,
+  ): Promise<ValidationError> {
+    try {
+      const competentAuthorities = await Promise.all(
+        instance.competentAuthorities.flatMap(
+          async (uri) => await this._bestuurseenheidRepository.findById(uri),
+        ),
       );
 
-      // TODO: refactor to clean up code and avoid duplication
-      try {
-        const competentAuthorities = await Promise.all(
-          instance.competentAuthorities.flatMap(
-            async (uri) => await this._bestuurseenheidRepository.findById(uri),
-          ),
-        );
+      const executingAuthorities = await Promise.all(
+        instance.executingAuthorities.flatMap(
+          async (uri) => await this._bestuurseenheidRepository.findById(uri),
+        ),
+      );
 
-        const executingAuthorities = await Promise.all(
-          instance.executingAuthorities.flatMap(
-            async (uri) => await this._bestuurseenheidRepository.findById(uri),
-          ),
-        );
+      const hasInvalidAuthority = competentAuthorities
+        .concat(executingAuthorities)
+        .some((authority) => !authority.isValidAuthority);
 
-        const hasInvalidAuthority = competentAuthorities
-          .concat(executingAuthorities)
-          .some((authority) => !authority.isValidAuthority);
-
-        if (hasInvalidAuthority) {
-          return [{ message: INACTIVE_AUTHORITY_ERROR_MESSAGE }];
-        }
-      } catch (e) {
-        if (e instanceof NotFoundError) {
-          return [{ message: INACTIVE_AUTHORITY_ERROR_MESSAGE }];
-        }
+      if (hasInvalidAuthority) {
+        return { message: INACTIVE_AUTHORITY_ERROR_MESSAGE };
       }
+    } catch (e) {
+      if (e instanceof NotFoundError) {
+        return { message: INACTIVE_AUTHORITY_ERROR_MESSAGE };
+      }
+    }
+  }
 
-      return this.tryValidateForPublish(instance);
+  private async validateSpatials(instance: Instance): Promise<ValidationError> {
+    try {
+      const spatials = await Promise.all(
+        instance.spatials.flatMap((uri) =>
+          this._spatialRepository.findById(uri),
+        ),
+      );
+
+      const hasExpiredSpatial = spatials.some((spatial) => spatial.isExpired);
+      if (hasExpiredSpatial) {
+        return { message: EXPIRED_SPATIAL_ERROR_MESSAGE };
+      }
+    } catch (e) {
+      if (e instanceof NotFoundError) {
+        return { message: EXPIRED_SPATIAL_ERROR_MESSAGE };
+      }
     }
   }
 
