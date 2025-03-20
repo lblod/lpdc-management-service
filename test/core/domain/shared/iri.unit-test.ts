e333a0652cd3f45ce5a51b611162503b44c401fc
onodrim <alexander.van.den.berghe@redpencil.io>
2025-03-20 20:48:03 +0100

    feat: allow to check format of authority URIs

--- test/core/domain/shared/iri.unit-test.ts
+++ test/core/domain/shared/iri.unit-test.ts
@@ -33,3 +33,54 @@ describe("constructing", () => {
     expect(iri.value).toEqual("http://some-value");
   });
 });
+
+describe("isValidAuthorityIri", () => {
+  test("it should return true for a valid OVO code IRI", () => {
+    const iri = new Iri("https://data.vlaanderen.be/id/organisatie/OVO123456");
+    expect(iri.isValidAuthorityIri).toBeTrue();
+  });
+
+  test("it should return false for an invalid OVO code URLs", () => {
+    let iri = new Iri("https://data.be/id/organisatie/OVO123456");
+    expect(iri.isValidAuthorityIri).toBeFalse();
+
+    iri = new Iri("https://data,vlaanderen,be/id/organisatie/OVO123456");
+    expect(iri.isValidAuthorityIri).toBeFalse();
+  });
+
+  test("it should return true for a valid administrative unit IRI", () => {
+    const iri = new Iri(
+      "http://data.lblod.info/id/bestuurseenheden/09f5b10fbd078fcb1e0e4910d32e47146a5eb31d8138dcbaec798309e64dd059",
+    );
+    expect(iri.isValidAuthorityIri).toBeTrue();
+  });
+
+  test("it should return false for a invalid administrative unit URLs", () => {
+    let iri = new Iri(
+      "http://data/id/bestuurseenheden/09f5b10fbd078fcb1e0e4910d32e47146a5eb31d8138dcbaec798309e64dd059",
+    );
+    expect(iri.isValidAuthorityIri).toBeFalse();
+
+    iri = new Iri(
+      "http://data!lblod+info/id/bestuurseenheden/09f5b10fbd078fcb1e0e4910d32e47146a5eb31d8138dcbaec798309e64dd059",
+    );
+    expect(iri.isValidAuthorityIri).toBeFalse();
+  });
+
+  test("it should return false for a administrative unit IRI with too short of an UUID", () => {
+    const iri = new Iri("http://data.lblod.info/id/bestuurseenheden/OVO002949");
+    expect(iri.isValidAuthorityIri).toBeFalse();
+  });
+
+  test("it should return false for an OVO code IRI with too few digits", () => {
+    const iri = new Iri("https://data.vlaanderen.be/id/organisatie/OVO12");
+    expect(iri.isValidAuthorityIri).toBeFalse();
+  });
+
+  test("it should return false for an OVO code IRI with too many digits", () => {
+    const iri = new Iri(
+      "https://data.vlaanderen.be/id/organisatie/OVO122456789",
+    );
+    expect(iri.isValidAuthorityIri).toBeFalse();
+  });
+});
